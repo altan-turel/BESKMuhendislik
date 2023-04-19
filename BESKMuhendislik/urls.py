9a52388234e497e1e15c42b1af1091924b6c8a58
bahadir samur <111216871+Bsamur@users.noreply.github.com>
2023-04-19 12:30:53 +0300

    Add files via upload

---
+++ BESKMuhendislik/urls.py
@@ -0,0 +1,36 @@
+"""BESKMuhendislik URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from BESK.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('iletisim', contact, name='contact'),
+    path('hakkimizda', about, name='about'),
+    path('referanslarimiz', referans, name='referans'),
+    path('faaliyet-alanlarimiz', faaliyet, name='faaliyet'),
+    path('proje-gorselleri', projeimage, name='projeimage'),
+    path('acad', acad, name='acad'),
+    path('3dsmax', dsmax, name='3dsmax'),
+    path('sta4cad', sta4cad, name='sta4cad'),
+    path('probina', probina, name='probina'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

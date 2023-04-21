10072464c6b136fd9edf4b4afc896abc059be7b0
bahadir samur <111216871+Bsamur@users.noreply.github.com>
2023-04-21 13:07:04 +0300

    Add files via upload

---
+++ BESK/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Job)
+admin.site.register(Birim)
+admin.site.register(Team)
+admin.site.register(Program)
+admin.site.register(Proje)
+admin.site.register(Faaliyet)
+admin.site.register(FaaliyetKonu)
+admin.site.register(Contact)
---
+++ BESK/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    name = models.CharField(("Görev Adı"), max_length=50)
+    def __str__(self):
+        return self.name
+    
+class Birim(models.Model):
+    name = models.CharField(("Görev Adı"), max_length=50)
+    def __str__(self):
+        return self.name
+    
+class Team(models.Model):
+    name = models.CharField(("Personel Adı"), max_length=50)
+    job = models.ForeignKey("Job", verbose_name=("Peronel Görevi"), on_delete=models.CASCADE)
+    image = models.FileField(("Personel Fotoğrafı"), upload_to=None, max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Program(models.Model):
+    name = models.CharField(("Program Adı"), max_length=50)
+    job = models.ForeignKey("Birim", verbose_name=("Program Görevi"), on_delete=models.CASCADE)
+    image = models.FileField(("Program Fotoğrafı"), upload_to=None, max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Proje(models.Model):
+    year = models.CharField(("Proje Yılı"), max_length=4)
+    team = models.ForeignKey("Team", verbose_name=("Proje Sorumlusu"), on_delete=models.CASCADE, blank=True, null=True)
+    ulke = models.CharField(("Ülke"), max_length=50, blank=True, null=True)
+    sehir = models.CharField(("Sehir"), max_length=50, blank=True, null=True)
+    name = models.CharField(("Proje Adı"), max_length=255)
+    who = models.CharField(("Proje Kimin?"), max_length=255)
+    start = models.CharField(("Proje Başlama Tarihi"), max_length=10)
+    finish = models.CharField(("Proje Bitiş Tarihi"), max_length=10)
+    price = models.DecimalField(("Proje Tutarı"), max_digits=9, decimal_places=2)
+    image = models.FileField(("Proje Görseli"), upload_to=None, max_length=100, blank=True, null=True)
+    image1 = models.FileField(("Proje Görseli"), upload_to=None, max_length=100, blank=True, null=True)
+    image2 = models.FileField(("Proje Görseli"), upload_to=None, max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Faaliyet(models.Model):
+    name = models.CharField(("Faaliyet Alanları"), max_length=255)
+    def __str__(self):
+        return self.name
+    
+class FaaliyetKonu(models.Model):
+    faaliyet = models.ForeignKey("Faaliyet", verbose_name=("Faaliyet Alanı"), on_delete=models.CASCADE)
+    name = models.CharField(("Faaliyet Konusu"), max_length=255)
+    def __str__(self):
+        return self.name
+    
+    
+class Contact(models.Model):
+    name = models.CharField(("Kullanıcı Adı"), max_length=255)
+    phone = models.CharField(("Kullanıcı Telefonu"), max_length=255, blank=True, null=True)
+    email = models.CharField(("Kullanıcı Email"), max_length=255)
+    message = models.TextField(("Kullanıcı Adı"))
+    
+    def __str__(self):
+        return self.name
